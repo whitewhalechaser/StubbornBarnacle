26459288e78678211d8c508270d64060127e4a9c
whitewhalechaser <106172522+whitewhalechaser@users.noreply.github.com>
2022-08-30 11:12:10 -0700

    First attempt at an setup script for permanent cluster servers

---
+++ current_make_pelican-v4.2/permanent_server_utilities/pre_pelican_setup_script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#change this path to the path on your machine
+PATH_TO_MOST_CURRENT_PELICAN_ISO="/home/user/stubborn_barnacle/current_make_pelican-v4.2/pelicanhpc-v4.2-xfce.iso"
+
+sudo mount -o loop  PATH_TO_MOST_CURRENT_PELICAN_ISO /usr/lib/live/mount/medium                      
+#need to make a directory first
+sudo mount /usr/lib/live/mount/medium/live/filesystem.squashfs  /home/user/temp_mount_for_squash -t squashfs -o loop               
+#cd /home/user/temp_mount_for_squash/usr/bin
+#sudo cp *pelican* /usr/bin/
+
+sudo cp /home/user/temp_mount_for_squash/usr/bin/*pelican* /usr/bin/
+sudo cp /home/user/temp_mount_for_squash/initrd.img /srv/tftp/      
+sudo cp /home/user/temp_mount_for_squash/vmlinuz /srv/tftp/
