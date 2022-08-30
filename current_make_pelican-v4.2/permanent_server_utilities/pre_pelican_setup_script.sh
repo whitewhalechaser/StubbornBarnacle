dce3d9754374589930416b3b520088ba660e587d
whitewhalechaser <106172522+whitewhalechaser@users.noreply.github.com>
2022-08-30 12:02:59 -0700

    tweaks to permanent server setup

--- current_make_pelican-v4.2/permanent_server_utilities/pre_pelican_setup_script.sh
+++ current_make_pelican-v4.2/permanent_server_utilities/pre_pelican_setup_script.sh
@@ -3,8 +3,9 @@
 #change this path to the path on your machine
 PATH_TO_MOST_CURRENT_PELICAN_ISO="/home/user/stubborn_barnacle/current_make_pelican-v4.2/pelicanhpc-v4.2-xfce.iso"
 
-sudo mount -o loop  PATH_TO_MOST_CURRENT_PELICAN_ISO /usr/lib/live/mount/medium                      
+sudo mount -o loop  $PATH_TO_MOST_CURRENT_PELICAN_ISO /usr/lib/live/mount/medium                      
 #need to make a directory first
+install -d /home/user/temp_mount_for_squash
 sudo mount /usr/lib/live/mount/medium/live/filesystem.squashfs  /home/user/temp_mount_for_squash -t squashfs -o loop               
 #cd /home/user/temp_mount_for_squash/usr/bin
 #sudo cp *pelican* /usr/bin/
